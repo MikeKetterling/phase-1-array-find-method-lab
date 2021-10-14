53aa6b3b9a082ecf47bd6932470f2f2f6d276d4a
Mike <mikekett4@yahoo.com>
2021-10-13 18:04:21 -0600

    commit tree c33b1272686f409bec56e3667d124a82b36f19f0

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
 
+function superbowlWin(arr) {
+    const response = arr.find(element => element.result === "W");
+    if (response === undefined) {
+        return response
+    } else {
+        return response.year;
+    }
+    console.log( response.year);
+}
+superbowlWin(record)
+
+
+console.log("this")
